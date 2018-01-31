769881f9e1c49beb63a6627cda73ac619f1602ce
Sang Diep <sangdngc60231@gmail.com>
2018-01-31 23:47:00 +0700

    update Image + intergrate Firebase

--- seavian/src/app/home/home.component.ts
+++ seavian/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database'; 
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  productsObservable: Observable<any[]>;
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.productsObservable = this.getProducts('/products');
+  }
+
+  getProducts(listPath): Observable<any[]> {
+    return this.db.list(listPath).valueChanges();
   }
 
 }
